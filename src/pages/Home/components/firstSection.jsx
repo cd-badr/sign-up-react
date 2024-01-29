5a33cc449b4f9091514d102fd59ed1d5a250f351
badr <badr@lionsgeek.ma>
2024-01-29 15:15:37 +0100

    login

--- src/pages/Home/components/firstSection.jsx
+++ src/pages/Home/components/firstSection.jsx
@@ -3,7 +3,17 @@ import './firstSection.scss'
 export const FirstSection = () => {
     return (
         <>
-        <h1>Hello World!</h1>
+        <div className="w-[500px]  bg-slate-50 rounded-xl flex justify-center items-center justify-evenly flex-col p-10 gap-10">
+            <h1 className='text-3xl border-b-2 border-zinc-300 w-full pb-3 text-center'>Login</h1>
+            <input className='w-full h-16 bg-slate-900 text-slate-50 rounded-xl ps-4 text-xl' type="text" placeholder='Your Email'/>
+            <input className='w-full h-16 bg-slate-900 text-slate-50 rounded-xl ps-4 text-xl' type="password" placeholder='Your Password'/>
+            <button className='text-xl bg-slate-400 text-slate-50 w-24 p-2 rounded-xl '>Login</button>
+            <div className='flex flex-row gap-2'>
+            <p>You do not have an account ? </p>
+            <button className='text-[#4f8399]'>Register</button>
+            </div>
+        </div>
+
         </>
     );
 }
